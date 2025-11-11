4c35e292f0854c4328a83fe42aed26588237b2a5
Diana <diana@vibrationresearch.cn>
2025-11-11 16:35:13 +0800

    强制指定登录成功跳转地址为 topics 页面

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ app_name = 'users'
 
 urlpatterns = [
     # 登录页面
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html', success_url='/learning_logs/topics/'), name='login'),
     # 注销
     path('logout/', views.logout_view, name='logout'),
     # 注册页面
